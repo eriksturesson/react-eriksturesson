2eb0d8bd334149c6b86e2966e2a225163ebab55f
Erik Sturesson <hej@eriksturesson.se>
2025-04-24 13:24:31 +0200

    no important change

--- src/components/PortfolioModal.tsx
+++ src/components/PortfolioModal.tsx
@@ -57,6 +57,7 @@ export default function PortfolioModal({ open, item, onClose }: Props) {
                 {Object.entries(item.tags).map(([category, tags], index) => (
                   <Box key={category}>
                     <Stack direction="row" spacing={1} flexWrap="wrap">
+                      s
                       {tags.map((tag: string) => (
                         <Chip
                           key={`${category}-${tag}`}
