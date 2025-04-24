4b5c38545d94c76670f68fafd56f11d10cfc164a
Erik Sturesson <hej@eriksturesson.se>
2025-04-24 17:56:57 +0200

    small style changes

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -28,9 +28,9 @@ export default function PortfolioCard({
         height="180"
         image={item.image.src}
         alt={"alt" in item.image ? item.image.alt : item.title}
-        sx={{ objectFit: "contain", pb: 2, pt: 2 }}
+        sx={{ objectFit: "contain", p: 2 }}
       />
-      <CardContent sx={{ flexGrow: 1, px: 3, py: 2 }}>
+      <CardContent sx={{ flexGrow: 1, px: 2, py: 2 }}>
         <Typography gutterBottom variant="h6">
           {item.title}
         </Typography>
