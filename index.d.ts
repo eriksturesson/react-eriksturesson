0ff66057bec54197bc08a238ad13d83b61d0cb26
Erik Sturesson (Beelink) <hej@eriksturesson.se>
2025-06-21 09:31:06 +0200

    imortant new vite.config.ts settings (max opening of files at the same time) and types if we build with typescript before vite

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "*.jpg";
+declare module "*.JPG";
+declare module "*.PNG";
+declare module "*.png";
+declare module "*.mp4";
+declare module "*.webm";
+declare module "*.webp";
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import fs from "fs";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Byggmappen (som tidigare var "build" i CRA)
+    outDir: "dist",
+    rollupOptions: {
+      // Begränsa max antal samtidiga async fs-operations (t.ex. 10)
+      maxParallelFileOps: 10,
+    },
   },
 });
