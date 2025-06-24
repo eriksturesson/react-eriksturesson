24b79395b39474e1e80a7b094e0ccbadd7a51cef
Erik Sturesson (Beelink) <hej@eriksturesson.se>
2025-06-24 16:07:06 +0200

    Showcasing 4 public IT-projects in About-section, github, npm and public website for stockholmcityaffarsnatverk.se

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,14 @@
 import GitHubIcon from "@mui/icons-material/GitHub"; // Kom ihåg att importera GitHub-ikonen
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Button } from "@mui/material";
+import { Button, Divider, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { motion } from "motion/react";
+import DescriptionIcon from "@mui/icons-material/Description";
 import { useEffect, useState } from "react";
 import { aboutData, aboutData2 } from "../data/aboutItems";
+import LanguageIcon from "@mui/icons-material/Language";
 import { LogoCarousel } from "./LogoCarousel";
 
 export default function About() {
@@ -29,64 +31,166 @@ export default function About() {
     <MotionBox
       initial={{ opacity: 0, scale: 0 }}
       whileInView={{ opacity: 1, scale: 1 }}
-      viewport={{ once: true, amount: 0.1 }}
+      viewport={{ once: true, amount: 0 }}
       transition={{
         duration: 1,
         scale: { type: "spring", visualDuration: 0.4, bounce: 0.3 },
       }}
     >
-      <Grid container width="100%" sx={{ display: "flex", flexDirection: "row" }}>
-        <Grid item xs={12} sm={12} sx={{ textAlign: "center" }}>
+      <Grid container width="100%" spacing={2} px={2} py={2} textAlign={"center"} justifyContent="center">
+        <Grid item xs={12}>
           <Typography
             variant={windowWidth < responsiveWidthLimit ? "h4" : "h2"}
             component={windowWidth < responsiveWidthLimit ? "h4" : "h2"}
-            mx={"2vw"}
-            mt={"2vw"}
-            textAlign="center"
           >
-            Produktägare | Fullstackutvecklare
+            Produktägare | Fullstack utvecklare
           </Typography>
-          <Typography variant="body1" component="cite" mx={"2vw"} mb={"2vw"} textAlign="center">
+          <Typography variant="body1" component="cite">
             Bygger och leder produktlösningar som skapar affärsvärde och gör idéer till verklighet.
           </Typography>
           <LogoCarousel logos={[...aboutData, ...aboutData2, ...aboutData, ...aboutData2]} direction="left" />
-          <Typography variant="h5" component="h5" mt={"4vw"} mb={"2vw"} mx={"2vw"} pl={2} pr={2}>
+          <Typography variant="h5" component="h5">
             Grundade och ledde Student Node AB, ett SaaS-företag som blev uppköpt av Poolia 2019. Specialiserad på att
             skapa tekniska lösningar som möter både affärs- och användarbehov.
           </Typography>
         </Grid>
-        <Grid item xs={12} md={6} sx={{ mt: 4 }}>
-          <Typography variant="h5" component="h5" textAlign="center">
-            Karriärinformation
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="info"
+            href={"https://www.linkedin.com/in/eriksturesson"}
+            target="_blank"
+            startIcon={<LinkedInIcon />}
+          >
+            Besök min LinkedIn
+          </Button>
+        </Grid>
+        <Divider />
+        <Grid item xs={12}>
+          <Typography
+            variant={windowWidth < responsiveWidthLimit ? "h4" : "h2"}
+            component={windowWidth < responsiveWidthLimit ? "h4" : "h2"}
+          >
+            Publik kod
           </Typography>
-          <Box display="flex" justifyContent="center" mt={2}>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5" component="h5">
+            Event Landing pages
+          </Typography>
+
+          <Typography component={"cite"}>
+            Skalbart, modernt, mobilanpassat eventsystem inklusive admin inloggning. Används av Stockholm City
+            Affärsnätverk
+          </Typography>
+          <Box>
             <Button
+              sx={{ margin: 1 }}
+              startIcon={<LanguageIcon />}
+              variant="contained"
+              color="info"
+              href="https://stockholmcityaffarsnatverk.se"
+              target="_blank"
+            >
+              Stockholm City Affärsnatverk
+            </Button>
+            <Button
+              sx={{ margin: 1 }}
               variant="contained"
               color="success"
-              href={"https://www.linkedin.com/in/eriksturesson"}
+              href={"https://github.com/eriksturesson/EventLandingPages"} // Byt ut denna URL med rätt GitHub-repository-länk
               target="_blank"
-              startIcon={<LinkedInIcon />}
+              startIcon={<GitHubIcon />}
             >
-              Min LinkedIn-profil
+              Event Landing Pages
             </Button>
           </Box>
         </Grid>
 
-        <Grid item xs={12} md={6} sx={{ mt: 4 }}>
-          <Typography variant="h5" component="h5" textAlign="center">
-            Koden för eriksturesson.se
-          </Typography>
-          <Box display="flex" justifyContent="center" mt={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5">NPM Paket</Typography>
+          <Typography component="cite">Paket för utvecklare i NodeJS / TypeScript</Typography>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6">backend-error</Typography>
+          <Box>
+            <img src="https://img.shields.io/npm/dy/backend-error?style=flat-square" alt="npm downloads" />
+          </Box>
+          <Box>
+            <img src="https://img.shields.io/npm/v/backend-error?style=flat-square" alt="npm version" />
+          </Box>
+          <Box>
             <Button
+              sx={{ margin: 1 }}
+              startIcon={<DescriptionIcon />}
+              variant="contained"
+              color="error"
+              href="https://www.npmjs.com/package/backend-error"
+              target="_blank"
+            >
+              Till NPM
+            </Button>
+            <Button
+              sx={{ margin: 1 }}
               variant="contained"
               color="success"
-              href={"https://github.com/eriksturesson/react-eriksturesson"} // Byt ut denna URL med rätt GitHub-repository-länk
+              href="https://github.com/eriksturesson/backendError"
               target="_blank"
               startIcon={<GitHubIcon />}
             >
-              eriksturesson.se på GitHub
+              GitHub
             </Button>
           </Box>
+        </Grid>
+
+        {/* error-drawings */}
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6">error-drawings</Typography>
+          <Box>
+            <img src="https://img.shields.io/npm/dy/error-drawings?style=flat-square" alt="npm downloads" />
+          </Box>
+          <Box>
+            <img src="https://img.shields.io/npm/v/error-drawings?style=flat-square" alt="npm version" />
+          </Box>
+          <Box>
+            <Button
+              sx={{ margin: 1 }}
+              startIcon={<DescriptionIcon />}
+              variant="contained"
+              color="error"
+              href="https://www.npmjs.com/package/error-drawings"
+              target="_blank"
+            >
+              Till NPM
+            </Button>
+            <Button
+              sx={{ margin: 1 }}
+              variant="contained"
+              color="success"
+              href="https://github.com/eriksturesson/errorDrawings"
+              target="_blank"
+              startIcon={<GitHubIcon />}
+            >
+              GitHub
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5" component="h5">
+            Denna hemsida
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="success"
+            href={"https://github.com/eriksturesson/react-eriksturesson"} // Byt ut denna URL med rätt GitHub-repository-länk
+            target="_blank"
+            startIcon={<GitHubIcon />}
+          >
+            eriksturesson.se
+          </Button>
         </Grid>
       </Grid>
     </MotionBox>
